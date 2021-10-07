028cb765e41ab4f2676843ff672c495f476a011a
lilanka <pathirage.lilanka@gmail.com>
2021-10-07 10:47:47 +0530

    2 done!

--- sorting.hh
+++ sorting.hh
@@ -14,5 +14,55 @@ public:
     } 
   }
 
+  // merge sort
+  template <typename T>
+  void merge_sort(std::vector<T>& lst) {
+    mergeSort<T>(lst, 0, lst.size()-1); 
+  }
 
+  template <typename T>
+  void merge(std::vector<T>& lst, int a, int c, int b) {
+    int tmp[b + 1];
+    int i = a;
+    int j = c + 1;
+    int k = 0;
+    
+    while (i <= c && j <= b) {
+      if (lst[i] <= lst[j])
+        tmp[k++] = lst[i++];
+      else
+        tmp[k++] = lst[j++];
+    } 
+    while (i <= c)
+      tmp[k++] = lst[i++];
+    while (j <= b)
+      tmp[k++] = lst[j++];
+    k--;
+    while (k >= 0) {
+      lst[k+a] = tmp[k];
+      k--; 
+    }
+  }
+
+  template <typename T> 
+  void mergeSort(std::vector<T>& lst, int a, int b) {  
+    int c; // mid value
+    if (a < b) {
+      c =  (a + b) / 2;
+      mergeSort(lst, a, c);
+      mergeSort(lst, c + 1, b);
+      merge(lst, a, c, b);
+    } 
+  }
+  
+  // bubble sort
+  template <typename T>
+  void bubble_sort(std::vector<T>& lst) {
+    for (int i=0; i < lst.size(); i++) {
+      for (int j=lst.size()-1; j > i+1; i--) {
+        if (lst[j] < lst[j - 1])
+          std::swap(lst[j], lst[j-1]); 
+      } 
+    } 
+  }
 };
--- testing.cc
+++ testing.cc
@@ -3,17 +3,33 @@
 
 #include "sorting.hh"
 #include "searching.hh"
+#include "honers_rule.hh"
 
 int main() { 
-  std::vector<int> x{5, 2, 4, 6, 1, 3};
+  SortingAlgorithms sa;
+  SearchingAlgorithms sea;
 
   std::cout << "========== insertion sort ===========" << std::endl;
-  SortingAlgorithms sa;
-  sa.insertion_sort<int>(x); 
-  for (int& i: x) { std::cout << i << "\n"; }
+  std::vector<int> x_insertion{5, 2, 4, 6, 1, 3};
+  sa.insertion_sort<int>(x_insertion); 
+  for (int& i: x_insertion) { std::cout << i << "\n"; }
 
   std::cout << "========== linear search ============" << std::endl;
-  SearchingAlgorithms sea;
+  std::vector<int> x{5, 2, 4, 6, 1, 3};
   int linear_search_index = sea.linear_search<int>(x, 4);
   std::cout << linear_search_index << std::endl;
+
+  std::cout << "========== merge sort ===========" << std::endl;
+  std::vector<int> x_merge{5, 2, 4, 6, 1, 3};
+  sa.merge_sort<int>(x_merge); 
+  for (int& i: x_merge) { std::cout << i << "\n"; }
+
+  std::cout << "========== bubble sort ===========" << std::endl;
+  std::vector<int> x_bubble{5, 2, 4, 6, 1, 3};
+  sa.merge_sort<int>(x_bubble); 
+  for (int& i: x_bubble) { std::cout << i << "\n"; }
+
+  std::cout << "========== Honer's rule ==========" << std::endl;
+  int hr_out = honers_rule<int>(x, x.size(), 0, 3); 
+  std::cout << hr_out << std::endl;
 }
